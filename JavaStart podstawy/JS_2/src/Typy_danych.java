b3084f33a5266428016fc3134c3966f9618baa1d
Dawid <32975922+ddolny1211@users.noreply.github.com>
2019-04-14 13:11:03 +0200

    Add files via upload

---
+++ JavaStart podstawy/JS_2/src/Typy_danych.java
@@ -0,0 +1,12 @@
+public class Typy_danych {
+    public static void main(String args[]){
+        System.out.print("Cześć Dawid, słyszałem, że masz ");
+        System.out.print(22);
+        System.out.println(" lat i uczysz się programowania, czy to prawda?");
+        System.out.println(true);
+        System.out.println("Czy Twoje inicjały to D D?");
+        System.out.print("tak, ");
+        System.out.println(true);
+
+    }
+}
---
+++ JavaStart podstawy/JS_3/src/Zmienne.java
@@ -0,0 +1,29 @@
+public class Zmienne {
+    public static void main(String args[]){
+ /*       String firstName = "Dawid";
+        String lastName = "Dolny";
+        int age = 22;
+        double height = 178.5;
+
+        final String pesel = "95111201257";
+
+        System.out.print("Cześć ");
+        System.out.println(firstName + " " + lastName);
+        System.out.println("Poniżej jest kilka informacji o Tobie: ");
+        System.out.print("Twój PESEL to: ");
+        System.out.println(pesel);
+        System.out.println("Masz " + age + " lat.");
+        System.out.println("Twój wzrost wynosi " + height + " cm.");
+        */
+
+        String marka = "Opel";
+        String model = "Astra H";
+        int rocznik = 2008;
+        int cena = 11300;
+        int moc = 90;
+
+        System.out.println("Posiadasz samochód " + marka + " " + model + " o mocy " + moc + " KM.");
+        System.out.println("Wyprodukowany został w " + rocznik + " roku. Jego cena obecnie wynosi " + cena + " zł.");
+
+    }
+}
